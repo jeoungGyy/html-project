00135a28a1252eca512c5b8497bfa2c9b5e82c85
jeoungGyy <limjungkoro@gmail.com>
2017-12-15 14:43:21 +0900

    HTML

---
+++ include/footer.js
@@ -0,0 +1,8 @@
+var str = '';
+
+str += '	<footer>';
+str += '		<p>푸터</p>';
+str += '	</footer>';
+
+
+document.write(str);
---
+++ include/head.js
@@ -0,0 +1,17 @@
+var str = '';
+
+str += '<meta http-equiv="X-UA-Compatible" content="IE=edge">';
+str += '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">';
+str += '<meta name="theme-color" content="#000000">';
+str += '<meta name="description" content="사이트 소개">';
+str += '<meta name="keywords" content="키워드입력, 키워드2, 키워드3">';
+str += '<link rel="stylesheet" href="../css/base.css">';
+str += '<link rel="stylesheet" href="../css/style.css">';
+str += '<script src="http://code.jquery.com/jquery-latest.js"></script>';
+str += '<script src="../js/ui.js"></script>';
+str += '<!--[if lt IE 9]>';
+str += '<script src="../js/html5.js"></script>';
+str += '<![endif]-->';
+
+
+document.write(str);
