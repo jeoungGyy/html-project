4455b4926f0108d8f27e83f190e02422de3b464d
jeoungGyy <limjungkoro@gmail.com>
2019-08-20 10:45:15 +0900

    Update head.js

--- include/head.js
+++ include/head.js
@@ -5,6 +5,11 @@ str += '<meta name="viewport" content="width=device-width, initial-scale=1, shri
 str += '<meta name="theme-color" content="#000000">';
 str += '<meta name="description" content="사이트 소개">';
 str += '<meta name="keywords" content="키워드입력, 키워드2, 키워드3">';
+str += '<meta property="og:type" content="website">';
+str += '<meta property="og:title" content="해당 링크의 제목">';
+str += '<meta property="og:description" content="해당 링크 설명">';
+str += '<meta property="og:image" content="썸네일 이미지, http:// 부터 시작하는 절대 주소를 써야합니다.">';
+str += '<meta property="og:url" content="링크 주소">';
 str += '<link rel="stylesheet" href="../css/base.css">';
 str += '<link rel="stylesheet" href="../css/style.css">';
 str += '<script src="http://code.jquery.com/jquery-latest.js"></script>';
